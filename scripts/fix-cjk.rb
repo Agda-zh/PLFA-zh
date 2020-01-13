e84c5e6b320adf7e99fd4de05bdce9f23d1a6c43
Oling Cat <olingcat@gmail.com>
2020-01-13 17:45:36 +0800

    Reviewed Induction. (#81)

--- scripts/fix-cjk.rb
+++ scripts/fix-cjk.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 filename = ARGV[0]
-# puts "Fixing line breaks for #{filename}"
+puts "Fixing line breaks for #{filename}"
 content = File.read(filename)
 REDUNDANT_LINE_BREAK_REGEX = /([\p{Han}\p{Po}]+)\n([\p{Han}]+)/u
 while content.gsub!(REDUNDANT_LINE_BREAK_REGEX, '\1\2')
